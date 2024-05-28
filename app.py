a944c3552bed18f65a9ae960ba732c820a2d8d4a
Rehaan1 <rehaanmazid@gmail.com>
2024-05-28 08:16:18 +0530

    refactor: refactor vector db code

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import streamlit as st
 import os
 
-from langchain_community.llms import Ollama
+# from langchain_community.llms import Ollama
 from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
-from langchain_community.vectorstores import FAISS
-from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_core.prompts import ChatPromptTemplate
+
+from vectorDB import create_vector_database
 
 from dotenv import load_dotenv
 
@@ -20,27 +18,6 @@ load_dotenv()
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 
-def create_vector_database():
-    """
-    This reads all runbook documents from a directory and creates a 
-    vector database.
-    """
-
-    # Intialize the vector database only if not already in session
-    # @TODO: Store Vector Database in a more permanent location
-    if "vectors" not in st.session_state:
-        message = st.empty()
-        message.text("Vectorizing Runbooks. Please wait.....")
-        st.session_state.embeddings = OpenAIEmbeddings()
-        st.session_state.loader = PyPDFDirectoryLoader("./runbooks")
-        st.session_state.docs = st.session_state.loader.load()
-        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,
-                                                                        chunk_overlap=200)
-        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs)
-        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents,
-                                                        st.session_state.embeddings)
-        message.text("Database Initialized")
-        
 st.title("Nebula Runbook LLM")
 st.subheader("A Runbook Assistant for Site Reliability Engineers")
 
@@ -59,11 +36,10 @@ prompt = ChatPromptTemplate.from_template(
     Question: {input}"""
 )
 
-input_prompt = st.text_input("What system issue are you facing?")
-
 if st.button("Initialize Runbooks"):
-    create_vector_database()
-    
+    create_vector_database(st)
+
+input_prompt = st.text_input("What system issue are you facing?")
 
 if input_prompt:
     with st.spinner("Thinking..."):
---
+++ vectorDB.py
@@ -0,0 +1,25 @@
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+
+def create_vector_database(st):
+    """
+    This reads all runbook documents from a directory and creates a 
+    vector database.
+    """
+
+    # Intialize the vector database only if not already in session
+    # @TODO: Store Vector Database in a more permanent location
+    if "vectors" not in st.session_state:
+        message = st.empty()
+        message.text("Vectorizing Runbooks. Please wait.....")
+        st.session_state.embeddings = OpenAIEmbeddings()
+        st.session_state.loader = PyPDFDirectoryLoader("./runbooks")
+        st.session_state.docs = st.session_state.loader.load()
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,
+                                                                        chunk_overlap=200)
+        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs)
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents,
+                                                        st.session_state.embeddings)
+        message.text("Database Initialized")
